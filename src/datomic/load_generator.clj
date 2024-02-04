c9925f5fc39e0867ed1b0de87dce181505b977bb
Darlei Soares <darlei.soares@nubank.com.br>
2024-02-04 18:29:45 -0300

    add draft for load-generator

--- src/datomic/load_generator.clj
+++ src/datomic/load_generator.clj
@@ -1,149 +1,90 @@
 (ns datomic.load-generator
   (:require
    [datomic.api :as d]
-   [datomic.common :refer [retry-fn with-nano-time log-retry await-derefs]]
-   [clojure.data.generators :as dgen]
-   [datomic.schema :as is])
+   [datomic.schema :as dschema]
+   [datomic.afinity :as aff])
   (:import
    (java.util UUID)))
-
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Activity Simulator
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn get-random-uuid
-  []
-  (UUID/randomUUID))
+(defn generate-customer-portifolio
+  "Returns a map of a customer + stocks"
+  [customer-id stock stock-code]
+  {:customer-portifolio/customer-id customer-id
+   :customer-portifolio/stock stock
+   :customer-portifolio/stock-code stock-code})
 
-(defn get-squiid
-  []
-  (d/squuid))
+(defn generate-customer-portifolio-partitioned
+  "Returns a map of a customer + stocks but partitioned"
+  [customer-id stock stock-code partition-id]
+  {:customer-portifolio/customer-id customer-id
+   :customer-portifolio/stock stock
+   :customer-portifolio/stock-code stock-code
+   :db/id (d/tempid (d/implicit-part partition-id))})
 
+(defn create-all-customers
+  "Creates n squuids and returns it"
+  [n-customers]
+  (take n-customers (repeatedly d/squuid)))
 
+(def all-customers (create-all-customers 100))
 
+(println all-customers)
 
+#_(let [db (d/db (d/connect db-uri))
+        query-map {:query '[:find ?e
+                            :in $
+                            :where
+                            [?e :stock/code]]
+                   :args [db]
+                   :io-context :dod/presentation}
+        {:keys [_ io-stats]} (d/query query-map)]
+  ;(println "Query Result:" ret)
+    (println "I/O Stats:" io-stats))
 
+(defn get-all-stocks
+  "Return all stocks availables to purchase
+   in the database"
+  [db]
+  (let [query-map {:query '[:find (pull ?e [:stock/code :db/id])
+                            :in $
+                            :where [?e :stock/code ?]]
+                   :args [db]}
+        ret (d/query query-map)]
+    ret))
 
+(def conn (d/connect "datomic:dev://localhost:4334/day-of-datomic"))
 
+(def all-stocks (get-all-stocks (d/db (d/connect "datomic:dev://localhost:4334/day-of-datomic"))))
 
-#_(defn pipeline
-  [{:keys [conn in-flight tps fell-behind-fn recording-fn op-count spin-sleep? done?]
-    :or   {done?       (atom false)
-           spin-sleep? false
-           op-count    Long/MAX_VALUE
-           fell-behind-fn #(println
-                            (let [ns (- (:now %) (:deadline %))
-                                  us (long (/ ns 1000))
-                                  ms (long (/ us 1000))]
-                              (format "Fell Behind by %s ns, %s us %s ms" ns us ms)))
-           recording-fn (fn [& _] (do (print ".") (flush)))}} txes]
-  (let [q (java.util.concurrent.LinkedBlockingQueue. (int in-flight))
-        target-ops-per-ms        (double (/ tps 1000))
-        target-ops-tick-ns       (long (/ 1000000 target-ops-per-ms))
-        target-ops-pos?          (> target-ops-per-ms 0)
-        sleep-until              (fn sleep-until [^long deadline]
-                                   (let [now (System/nanoTime)]
-                                     (when-not (< now deadline)
-                                       (fell-behind-fn {:now now :deadline deadline})))
-                                   (while (< (System/nanoTime) deadline)
-                                     (when-not spin-sleep?
-                                       (java.util.concurrent.locks.LockSupport/parkNanos (- deadline (System/nanoTime))))))
-        throttle-nanos           (fn throttle-nanos [^long start-time-nanos ^long ops-done]
-                                   (let [deadline (+ start-time-nanos (* ops-done target-ops-tick-ns))]
-                                     (when target-ops-pos? (sleep-until deadline))
-                                     deadline))]
-    {:result-future (future
-                      (loop [{:keys [tx fut] :as current} (.take q)]
-                        (when-not (= current :done)
-                          (if fut
-                            (if-let [res (try
-                                           (deref fut 10000 nil)
-                                           (catch Throwable t nil))]
-                              (let [completed (System/nanoTime)]
-                                (recording-fn (assoc current
-                                                     :res res
-                                                     :completed completed)))
-                              (println "failure after submission. insert retry logic here for" tx))
-                            (println "failure prior to submission. insert retry logic here for" tx))
-                          (recur (.take q))))
-                      :done)
-     :submit-future (future
-                      (let [start-time-nanos (System/nanoTime)]
-                        (loop [txes     txes
-                               ops-done (long 0)]
-                          (when (and
-                                 (seq txes)
-                                 (not @done?)
-                                 (and op-count (< ops-done op-count)))
-                            ;; Do Work
-                            (.put q {:started   (System/nanoTime)
-                                     :scheduled (+ start-time-nanos
-                                                   (* ops-done target-ops-tick-ns))
-                                     :tx        (first txes)
-                                     :fut       (try
-                                                  (d/transact-async conn (first txes))
-                                                  (catch Throwable t nil))})
-                            (throttle-nanos start-time-nanos (inc ops-done))
-                            (recur (next txes) (inc ops-done))))
-                        (.put q :done))
-                      :done)}))
+;(println (:stock/code (ffirst all-stocks)))
 
+(def one-stock (first (rand-nth all-stocks)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Activity Generators
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-#_(defn rand-nth
-  "Replacement of core/rand-nth (and generators/rand-nth) that allows
-  control of the randomization basis (through binding *rnd*) and the
-  distribution (via dist).
+(println (generate-customer-portifolio (rand-nth all-customers) (:stock/code one-stock) (:db/id one-stock)))
 
-  dist defaults to (partial uniform 0 (count coll))."
-  ([coll]
-   (rand-nth (partial dgen/uniform 0 (count coll)) coll))
-  ([dist coll]
-   (nth coll (dist))))
+(defn transact-one-example
+  [txes
+   db]
+  (d/transact db txes))
+
+(do
+  (println "---------------------------------------------------")
+  (doseq [_ (range 2)]
+    (let [c-id (rand-nth all-customers)
+          one-stock (first (rand-nth all-stocks))
+          stock-code (:stock/code one-stock)
+          partition-key (aff/hash-uuid (rand-nth all-customers))]
+      (clojure.pprint/pprint (generate-customer-portifolio c-id (:db/id one-stock) stock-code))
+      (clojure.pprint/pprint (generate-customer-portifolio-partitioned c-id (:db/id one-stock) stock-code partition-key))
+      (aff/hash-uuid (rand-nth all-customers))
+      (println "############################################")))
+  (println "---------------------------------------------------"))
 
 
 
-#_(defn load-txes
-  [conn batch conc txes]
-  (->> txes
-       (partition-all batch)
-       (pipeline {:conn conn :in-flight conc :tps conc})
-       (vals)
-       (await-derefs)))
 
-#_(defn run
-  [{:keys [uri seed
-           accounts
-           pixes balance-conc balance-batch tps
-           in-flight]
-    :or {seed 42 balance-batch 1000 balance-conc 10 tps 2 in-flight 100}}]
-  (d/create-database uri)
-  ;(transact-schema uri)
-  (binding [dgen/*rnd* (java.util.Random. seed)]
-    (let [conn (d/connect uri)
-          _ (->> #(gen-savings-account)
-                 (repeatedly accounts)
-                 (seque 100)
-                 (load-txes conn balance-batch balance-conc))
-          savings-account-id-pool (into [] (map :e)
-                                        (seq (d/datoms (d/db conn) :avet :savings-account/id)))
-          dist (partial dgen/uniform 0 (count savings-account-id-pool))]
-      (->> #(gen-pix-request-event (d/db conn) dist savings-account-id-pool)
-           (repeatedly (long pixes))
-           (map #(activity->tx-data (d/db conn) %))
-           (pipeline {:conn conn
-                      :in-flight in-flight
-                      :tps tps})
-           (vals)
-           (await-derefs)))))
 
-#_(comment
-  (run {:uri "datomic:dev://localhost:4334/day-of-datomic"
-        :stocks 1000
-        :accounts 10
-        :tps 10}))
